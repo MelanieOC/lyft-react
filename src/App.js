7c467fb4ccc06320ce3420a8568673852ca31fa0
MelanieOC <melanie.ocharan@gmail.com>
2017-10-10 11:10:14 -0500

    maquetado completo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import logo from './img/logo-pink.png';
+import usuario from './img/usuario.png';
 import './App.css';
-//https://maps.googleapis.com/maps/api/js?key=AIzaSyCdHQoVJGHGk8lG3ay1a7OXTdgMSE1szfM&libraries=places
+//https://maps.googleapis.com/maps/api/js?key=AIzaSyCl5FxNRuSN9tRMoZFJsH_q511Zt6DBVH4&libraries=places
 //AIzaSyCl5FxNRuSN9tRMoZFJsH_q511Zt6DBVH4
-import SearchBox from "react-google-maps/lib/components/places/SearchBox";
+//import { compose, withProps, lifecycle } from "recompose";
 import {
   withScriptjs,
   withGoogleMap,
@@ -11,77 +13,83 @@ import {
   Marker,
 } from "react-google-maps";
 
-const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+const MyMapComponent = withScriptjs(withGoogleMap(props =>
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={props.center}
+    defaultZoom={15}
+    center={props.center}
   >
     <Marker
       position={props.center}
     />
   </GoogleMap>
 ));
-const MyMapComponent = withScriptjs(withGoogleMap(props =>
-  <GoogleMap
-    ref={props.onMapMounted}
-    defaultZoom={15}
-    center={props.center}
-    onBoundsChanged={props.onBoundsChanged}
-  >
-    <SearchBox
-      ref={props.onSearchBoxMounted}
-      bounds={props.bounds}
-      //controlPosition={google.maps.ControlPosition.TOP_LEFT}
-      onPlacesChanged={props.onPlacesChanged}
-    >
-      <input
-        type="text"
-        placeholder="Customized your placeholder"
-        style={{
-          boxSizing: `border-box`,
-          border: `1px solid transparent`,
-          width: `240px`,
-          height: `32px`,
-          marginTop: `27px`,
-          padding: `0 12px`,
-          borderRadius: `3px`,
-          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-          fontSize: `14px`,
-          outline: `none`,
-          textOverflow: `ellipses`,
-        }}
-      />
-    </SearchBox>
-    <Marker
-    position={props.center}
-  />
-  </GoogleMap>
-));
-class MyFancyComponent extends React.PureComponent {
+
+class App extends Component {
   state = {
     center: { lat: -16.3988900, lng: -71.5350000 },
     bounds: null
   }
-
-  render() {
-    return (
-      <MapWithAMarker
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCl5FxNRuSN9tRMoZFJsH_q511Zt6DBVH4&v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-        center={this.state.center}
-      />
-    )
-  }
-}
-
-class App extends Component {
   render() {
     return (
       <div>
-        <MyFancyComponent/>
+        <header id="mapa_header">
+          <img className="img-responsive" id='usuario_logo' src={usuario} alt="" />
+          
+          <img className="img-responsive" src={logo} alt="" />
+          <span className="fa-stack fa-lg fa-2x">
+            <i className="fa fa-circle fa-stack-2x"></i>
+            <i className="fa fa-gift fa-stack-1x fa-inverse"></i>
+          </span>
+          <div id="mySidenav" className="sidenav">
+            <a href="javascript:void(0)" className="closebtn">&times;</a>
+            <div id="datos_usuario">
+            </div>
+          </div>
+        </header>
+        <MyMapComponent
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCl5FxNRuSN9tRMoZFJsH_q511Zt6DBVH4&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ position: 'absolute', height: `100%`, width: '100%' }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          center={this.state.center}
+        />
+        <div className="container">
+          <div className="row">
+            <div id="menu_mapa" className="col-sm-12 col-xs-12">
+              <div className="ocultar">
+                <div className="well precio">
+                  <img src="assets/img/carro.png" alt="carro" />
+                  <span><h4>Lyft</h4><p>Fast ride 4 seats</p></span>
+                </div>
+                <div className="well precio">
+                  <div className="text-center">
+                    <p id="precio"></p>
+                    <p>Price stimated</p>
+                  </div>
+                </div>
+              </div>
+              <div id="origen" className="form-control"></div>
+              <input type="text" id="destino" className="form-control" />
+              <button type="button" className="btn" id="ruta">Set pickup</button>
+              <button type="button" className="btn" id="solicitar" data-toggle="modal" data-target="#myModal">Request Lyft</button>
+            </div>
+          </div>
+        </div>
+        <div id="myModal" className="modal fade" role="dialog">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
+                <h4 className="modal-title text-center">Tu lyft está en camino</h4>
+              </div>
+              <div className="modal-body">
+                <button className="btn" id="otro_viaje">Otro viaje</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
     );
   }
 }

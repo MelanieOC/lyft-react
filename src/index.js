c32efc18385a6ac12030639d5b60011a92511119
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-10-10 13:24:09 -0500

    sin index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import SingUp from './SingUp';
-//import Form from './form';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<SingUp/>, document.getElementById('root'));
-registerServiceWorker();
